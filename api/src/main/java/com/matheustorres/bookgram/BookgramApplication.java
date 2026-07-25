f6c2de928220fa5aae91e380d903e9493130adbd
matheustorresdev97 <matheustorresdev@gmail.com>
2026-07-25 18:37:31 -0300

    feat: inicializar estrutura do projeto Spring Boot com Maven Wrapper e configuração básica

---
+++ api/src/main/java/com/matheustorres/bookgram/BookgramApplication.java
@@ -0,0 +1,13 @@
+package com.matheustorres.bookgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookgramApplication.class, args);
+	}
+
+}
